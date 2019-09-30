95ab1ac109a0225b8c754a5bb9ddbd1f7a08f42b
Jessica de Lima Fileto <jessica.lima@gupy.com.br>
2019-09-30 11:43:18 -0300

    Create jest and enzyme configs

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: [
+      '<rootDir>/src'
+  ],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupEnzyme.ts'],
+};
---
+++ src/setupEnzyme.ts
@@ -0,0 +1,4 @@
+import 'jsdom-global/register';
+import { configure } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+configure({ adapter: new EnzymeAdapter() });
