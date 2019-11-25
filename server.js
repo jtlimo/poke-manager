b05df1f7c020de9b09a9880f4dc9fc71a5f0fb15
Jessica de Lima Fileto <jessica.lima@gupy.com.br>
2019-11-25 14:03:16 -0300

    comment server.js file

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const fastify = require('fastify')()
-
-fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
-})
-
-const start = async () => {
-  try {
-    await fastify.listen(3000)
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-}
-start()
+// const fastify = require('fastify')()
+//
+// fastify.get('/', async (request, reply) => {
+//   return { hello: 'world' }
+// })
+//
+// const start = async () => {
+//   try {
+//     await fastify.listen(3000)
+//   } catch (err) {
+//     fastify.log.error(err)
+//     process.exit(1)
+//   }
+// }
+// start()
