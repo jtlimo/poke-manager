998ff72b13ada8d63bb0589801953f082b01052d
codefactor-io <support@codefactor.io>
2019-12-15 21:34:31 +0000

    [CodeFactor] Apply fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,11 @@
 module.exports = {
-  entry: __dirname+'/src/app.tsx',
+  entry: __dirname + '/src/app.tsx',
   output: {
-    path: __dirname+'/src',
-    filename: 'build/app.bundle.js'
+    path: __dirname + '/src',
+    filename: 'build/app.bundle.js',
   },
   module: {
-    rules: [
-      { test: /\.(ts|tsx)$/, loader: 'awesome-typescript-loader' }
-    ]
+    rules: [{ test: /\.(ts|tsx)$/, loader: 'awesome-typescript-loader' }],
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
