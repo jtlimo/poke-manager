cd850d6413a691b71ee94074191c832c5d024794
Jessica de Lima Fileto <jessica.lima@gupy.com.br>
2020-02-01 21:06:20 -0300

    Modifies structure to feature based
    - Update lintstagedrc

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-import PokeManager from '../src/containers/PokeManager/PokeManager';
+import PokeManager from './PokeManager';
 
 ReactDom.render(<PokeManager />, document.getElementById('src'));
