477fb477e86a9ed8dbea5cad5e4d3b533442b0b5
Jessica de Lima Fileto <jessica.lima@gupy.com.br>
2020-02-28 20:37:06 -0300

    chore: configure lint-staged and fix jest tests

--- src/__tests__/PokeManager.test.tsx
+++
@@ -1,9 +0,0 @@
-// import PokeManager from './PokeManager';
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-//
-// describe('PokeManager', () => {
-//   it('does renders correctly', () => {
-//     ReactDOM.render(<PokeManager />, document.getElementById('src'));
-//   });
-// });
